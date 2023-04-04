a9012df45eefef85476ac9aba2ef4d86045bffbd
sayalisontakke <98416681+sayalisontakke@users.noreply.github.com>
2023-04-04 23:02:33 +0530

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,45 @@
+import datetime
+
+def Time():
+    time =datetime.datetime.now().strftime("%H:%m")
+    Say(time)
+    
+def Date():
+    time =datetime.datetime.date.today()
+    Say(date)
+    
+def Day():
+    day =datetime.datetime.now().strftime("%A")
+    Say(day)
+    
+#Day()
+    
+def NonInputExecution(query):
+    query = str(query)
+    if "time" in query:
+        Time()
+        
+    elif "date" in query:
+        Date()
+        
+    elif "day" in query:
+        Day()
+        
+def NonInputExecution(query):
+    
+#     2 Input
+# eg. google search, wikipedia
+def InputExecution(tag,query)
+    if "wikipedia" in tag:
+        name = str(query).replace("who is", "").replace("about", "").replace("what is", "").replace("wikipedia", "")
+        import wikipedia
+        result = wikipedia.summary(name)
+        Say(result)
+        
+        
+    elif "google" in tag:
+        query = str(query).replace("google", "")
+        query = query.replace("search", "")
+        import pywhatkit
+        pywhatkit.search(query)
+
